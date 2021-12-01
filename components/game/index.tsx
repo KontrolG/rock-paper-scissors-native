32192c0dd1039e653c0c5a5bfe0705f8221509e3
German Solano <kontrolg1239@gmail.com>
2021-12-01 06:33:18 -0400

    feat(<Game />): :sparkles: Add game elements and logic

--- components/game/index.tsx
+++ components/game/index.tsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import { PicksPentagon } from "../picks-pentagon";
 import { Header } from "../header";
 import { RulesButton } from "../rules-button";
+import useStore from "../../lib/hooks/store";
+import Maybe from "../maybe";
+import { PICKS_NAMES } from "../../lib/types/picks";
+import { PicksSelected } from "../picks-selected";
+import { GameResult } from "../game-result";
 
 function Game() {
+  const youHadPicked = useStore(({ youPick }) => youPick !== PICKS_NAMES.NONE);
+  const houseHadPicked = useStore(
+    ({ housePick }) => housePick !== PICKS_NAMES.NONE
+  );
+
   return (
     <View style={styles.container}>
       <Header style={styles.header} />
-      <PicksPentagon />
+      <Maybe it={!youHadPicked}>
+        <PicksPentagon />
+      </Maybe>
+      <Maybe it={youHadPicked}>
+        <PicksSelected />
+        <Maybe it={houseHadPicked}>
+          <GameResult style={styles.gameResult} />
+        </Maybe>
+      </Maybe>
       <RulesButton style={styles.rulesButton} />
     </View>
   );
@@ -26,6 +44,9 @@ const styles = StyleSheet.create({
   },
   rulesButton: {
     marginTop: "auto"
+  },
+  gameResult: {
+    marginTop: 64
   }
 });
 
